85f14f14a205b7faa5c848d66509815482a527cf
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-02-01 22:31:06 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName, userEmail){
+  const requestBody = {
+    name: userName,
+    email: userEmail
+  }
+
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  }
+
+  return fetch('http://localhost:3000/users', configObj)
+  .then(response => response.json())
+  .then(data => {
+    const h2 = document.createElement('h2')
+    h2.textContent = data.id 
+    document.body.appendChild(h2)
+  })
+  .catch(error => {
+    const h2 = document.createElement('h2')
+    h2.textContent = error.message
+    document.body.appendChild(h2)
+  })
+}
